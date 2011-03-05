9d69a69a19ca746718c8d48a462ddb27b4cae0bf
niccholaspage <niccholaspage@gmail.com>
2011-03-05 11:35:54 -0500

    0.2.1 commit + fix

--- src/com/niccholaspage/FakeMessage/FakeMessage.java
+++ src/com/niccholaspage/FakeMessage/FakeMessage.java
@@ -1,100 +1,60 @@
-//The Package
 package com.niccholaspage.FakeMessage;
-//All the imports
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import org.bukkit.event.Event;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.PluginManager;
-import com.nijikokun.bukkit.Permissions.Permissions;
+
 import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+import java.io.File;
+import org.bukkit.event.Event;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
 
-/**
- * FakeMessage for Bukkit
- *
- * @author niccholaspage
- */
-//Starts the class
-public class FakeMessage extends JavaPlugin{
-	//Links the FakeMessagePlayerListener
-	private final FakeMessagePlayerListener playerListener = new FakeMessagePlayerListener(this);
-    public static PermissionHandler Permissions = null;
-    
-   
-	//public FakeMessage(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader)
-    //{
-     //   super(pluginLoader, instance, desc, folder, plugin, cLoader);
-      //}
+public class FakeMessage extends JavaPlugin
+{
+  private final FakeMessagePlayerListener playerListener = new FakeMessagePlayerListener(this);
+  public static PermissionHandler Permissions = null;
 
-	@Override
-	//When the plugin is disabled this method is called.
-	public void onDisable() {
-		//Print "FakeMessage Disabled" on the log.
-		System.out.println("FakeMessage Disabled");
-		
-	}
-    public void readConfig() {
-    	Configuration _config = new Configuration(new File("plugins/FakeMessage.yml"));
+  public void onDisable()
+  {
+    System.out.println("FakeMessage Disabled");
+  }
 
-    	_config.load();
-    	 //Check if file exists
-    	File file=new File("plugins/FakeMessage.yml");
-    	if (!file.exists()){
-    	      try{
-    	    	    // Create file 
-    	    	    FileWriter fstream = new FileWriter("plugins/FakeMessage.yml");
-    	    	        BufferedWriter out = new BufferedWriter(fstream);
-    	    	    out.write("FakeMessage:\n");
-    	    	    out.write("    messageformat: '<name> message'\n");
-    	    	    out.write("    joingame: '&ename has joined the game.'\n");
-    	    	    out.write("    leavegame: '&ename has left the game.'\n");
-    	    	    out.write("    defaultgroupname: 'Default'\n");
-    	    	    //Close the output stream
-    	    	    out.close();
-    	    	    }catch (Exception e){//Catch exception if any
-    	    	      System.out.println("FakeMessage could not write default config file.");
-    	    	    }
-    	}
-    	// Reading from yml file
-    	String messageformat = _config.getString("FakeMessage.messageformat", "<+name> +message");
-    	String joingame = _config.getString("FakeMessage.joingame", "&e+name has joined the game.");
-    	String leftgame = _config.getString("FakeMessage.leavegame", "&e+name has left the game.");
-    	String dgroup = _config.getString("FakeMessage.defaultgroupname","Default");
-    	messageformat = messageformat.replaceAll("&", "¤");
-    	joingame = joingame.replaceAll("&", "¤");
-    	leftgame = leftgame.replaceAll("&", "¤");
-    	FakeMessagePlayerListener.setMessages(messageformat, joingame, leftgame, dgroup);
-        }
-    public void setupPermissions() {
-    	Plugin test = this.getServer().getPluginManager().getPlugin("Permissions");
+  public void readConfig() {
+    Configuration _config = new Configuration(new File("plugins/FakeMessage.yml"));
 
+    _config.load();
 
-    	if(FakeMessage.Permissions == null) {
-    	    if(test != null) {
-    		FakeMessage.Permissions = ((Permissions)test).getHandler();
-    	    } else {
-    		System.out.println("Permission system not enabled. Disabling FakeMessage.");
-    		this.getServer().getPluginManager().disablePlugin(this);
-    	    }
-    	}
-        }
+    String messageformat = _config.getString("FakeMessage.messageformat", "<name> message");
+    String joingame = _config.getString("FakeMessage.joingame", "&ename has joined the game.");
+    String leftgame = _config.getString("FakeMessage.leavegame", "&ename has left the game.");
+    messageformat = messageformat.replaceAll("&", "¤");
+    joingame = joingame.replaceAll("&", "¤");
+    leftgame = leftgame.replaceAll("&", "¤");
+    FakeMessagePlayerListener.setMessages(messageformat, joingame, leftgame);
+  }
+  public void setupPermissions() {
+    Plugin test = getServer().getPluginManager().getPlugin("Permissions");
 
-	@Override
-	//When the plugin is enabled this method is called.
-	public void onEnable() {
-		//Create the pluginmanage pm.
-		PluginManager pm = getServer().getPluginManager();
-		//Create PlayerCommand listener
-	    pm.registerEvent(Event.Type.PLAYER_COMMAND, this.playerListener, Event.Priority.Normal, this);
-       //Get the infomation from the yml file.
-        PluginDescriptionFile pdfFile = this.getDescription();
-        //Print that the plugin has been enabled!
-        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
-        readConfig();
-        setupPermissions();
-	}
+    if (Permissions == null)
+      if (test != null) {
+        Permissions = ((Permissions)test).getHandler();
+      } else {
+        System.out.println("Permission system not enabled. Disabling FakeMessage.");
+        getServer().getPluginManager().disablePlugin(this);
+      }
+  }
+
+  public void onEnable()
+  {
+    PluginManager pm = getServer().getPluginManager();
+
+    pm.registerEvent(Event.Type.PLAYER_COMMAND_PREPROCESS, this.playerListener, Event.Priority.Normal, this);
+
+    PluginDescriptionFile pdfFile = getDescription();
+
+    System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
+    readConfig();
+    setupPermissions();
+  }
 }
--- src/com/niccholaspage/FakeMessage/FakeMessagePlayerListener.java
+++ src/com/niccholaspage/FakeMessage/FakeMessagePlayerListener.java
@@ -1,152 +1,94 @@
-//The Package
 package com.niccholaspage.FakeMessage;
-//All the imports
-import org.bukkit.ChatColor;
+
+import com.nijiko.permissions.PermissionHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerListener;
-import com.nijiko.permissions.PermissionHandler;
-//import org.bukkit.event.server.ServerEvent;
-//Starts the class FakeMessagePlayer listener
-public class FakeMessagePlayerListener extends PlayerListener{
-	 public static FakeMessage plugin;
-	 public static PermissionHandler Permissions = null;
-	  public FakeMessagePlayerListener(FakeMessage instance) {
-	        plugin = instance;
-	    }
-	  public static String arrayToString(String[] a, String separator) {
-		    StringBuffer result = new StringBuffer();
-		    if (a.length > 0) {
-		        result.append(a[0]);
-		        for (int i=1; i<a.length; i++) {
-		        	if (!(a[i].equals(""))){
-			            result.append(separator);
-			            result.append(a[i]);
-		        	}
-		        }
-		    }
-		    return result.toString();
-		}
-	  private static String messageFormat;
-	  private static String joingame;
-	  private static String leavegame;
-	  private static String dgroup;
 
-	  public static void setMessages(String mf, String jg, String lg, String dg) {
-	   messageFormat = mf;
-	   joingame = jg;
-	   leavegame = lg;
-	   dgroup = dg;
-	  }
-	  public static String removeCharAt(String s, int pos) {
-		   StringBuffer buf = new StringBuffer( s.length() - 1 );
-		   buf.append( s.substring(0,pos) ).append( s.substring(pos+1) );
-		   return buf.toString();
-		}
-	  //This method is called whenever a player uses a command.
-	  public void onPlayerCommand(PlayerChatEvent event) {
-		  //Make the message a string.
-			String[] split = event.getMessage().split(" ");
-			//Get the player that talked.
-			//Player player = event.getPlayer();
-			//If the first part of the string is /FakeMessage or /b then do this.
-			if (split[0].equalsIgnoreCase("/fsay")) {
-				Player player = event.getPlayer();
-				if (!FakeMessage.Permissions.has(player, "fakemessage.say")) {
-				    return;
-				}
-				if (!(split.length >= 3)){
-					player.sendMessage(ChatColor.RED + "/fsay playername message");
-					return;
-				}
-				split[0] = "";
-				String theguy = split[1];
-				split[1] = "";
-				String message = arrayToString(split, " ");
-				message = removeCharAt(message, 0);
-				String text = messageFormat;
-				text = text.replaceAll("\\+name",theguy);
-				text = text.replaceAll("\\+message", message);
-				text = text.replaceAll("\\+group", dgroup);
-				//plugin.getServer().broadcastMessage("[Default] " + theguy + ": " + message);
-				plugin.getServer().broadcastMessage(text);
-				event.setCancelled(false);
-			}
-			/*if ((split[0].equalsIgnoreCase("/groupfakemessage"))
-					|| (split[0].equalsIgnoreCase("/gsay"))) {
-				Player player = event.getPlayer();
-				if (!FakeMessage.Permissions.has(player, "fakemessage.gsay")) {
-				    return;
-				}
-				split[0] = "";
-				String thegroup = split[1];
-				split[1] = "";
-				String theguy = split[2];
-				split[2] = "";
-				String message = arrayToString(split," ");
-				message = removeCharAt(message, 0);
-				String text = messageFormat;
-				text = text.replaceAll("name",theguy);
-				text = text.replaceAll("message", message);
-				text = text.replaceAll("group", thegroup);
-				plugin.getServer().broadcastMessage(text);
-				event.setCancelled(false);
-			}*/
-			if (split[0].equalsIgnoreCase("/fcsay")) {
-				Player player = event.getPlayer();
-				if (!FakeMessage.Permissions.has(player, "fakemessage.csay")) {
-				    return;
-				}
-				if (!(split.length >= 2)){
-					player.sendMessage(ChatColor.RED + "/fcsay message");
-					return;
-				}
-				split[0] = "";
-				String message = arrayToString(split, " ");
-				message = removeCharAt(message, 0);
-				message = message.replaceAll("~","¤");
-				plugin.getServer().broadcastMessage(message);
-			}
-			if ((split[0].equalsIgnoreCase("/fjoin"))
-					|| (split[0].equalsIgnoreCase("/fj"))) {
-				Player player = event.getPlayer();
-				if (!FakeMessage.Permissions.has(player, "fakemessage.join")) {
-				    return;
-				}
-				if (!(split.length >= 2)){
-					if (split[0].equalsIgnoreCase("/fjoin")){
-						player.sendMessage(ChatColor.RED + "/fjoin name");
-					}else {
-						player.sendMessage(ChatColor.RED + "/fj name");
-					}
-					return;
-				}
-				String theguy = split[1];
-				String text = joingame;
-				text = text.replaceAll("\\+name", theguy);
-				plugin.getServer().broadcastMessage(text);
-				event.setCancelled(false);
-			}
-			if ((split[0].equalsIgnoreCase("/fleave"))
-					|| (split[0].equalsIgnoreCase("/fl"))) {
-				Player player = event.getPlayer();
-				if (!FakeMessage.Permissions.has(player, "fakemessage.leave")) {
-				    return;
-				}
-				if (!(split.length >= 2)){
-					if (split[0].equalsIgnoreCase("/fleave")){
-						player.sendMessage(ChatColor.RED + "/fleave name");
-					}else {
-						player.sendMessage(ChatColor.RED + "/fl name");
-					}
-					return;
-				}
-				String theguy = split[1];
-				String text = leavegame;
-				text = text.replaceAll("\\+name", theguy);
-				plugin.getServer().broadcastMessage(text);
-				event.setCancelled(false);
-			}
+public class FakeMessagePlayerListener extends PlayerListener
+{
+  public static FakeMessage plugin;
+  public static PermissionHandler Permissions = null;
+  private static String messageFormat;
+  private static String joingame;
+  private static String leavegame;
 
-		}
+  public FakeMessagePlayerListener(FakeMessage instance)
+  {
+    plugin = instance;
+  }
+  public static String arrayToString(String[] a, String separator) {
+    StringBuffer result = new StringBuffer();
+    if (a.length > 0) {
+      result.append(a[0]);
+      for (int i = 1; i < a.length; i++) {
+        if (a[i].equals("")) {
+          continue;
+        }
+        result.append(separator);
+        result.append(a[i]);
+      }
+    }
+    return result.toString();
+  }
+
+  public static void setMessages(String mf, String jg, String lg)
+  {
+    messageFormat = mf;
+    joingame = jg;
+    leavegame = lg;
+  }
+  public static String removeCharAt(String s, int pos) {
+    StringBuffer buf = new StringBuffer(s.length() - 1);
+    buf.append(s.substring(0, pos)).append(s.substring(pos + 1));
+    return buf.toString();
+  }
+
+  public void onPlayerCommandPreprocess(PlayerChatEvent event)
+  {
+    String[] split = event.getMessage().split(" ");
+
+    if ((split[0].equalsIgnoreCase("/fakemessage")) || 
+      (split[0].equalsIgnoreCase("/say"))) {
+      Player player = event.getPlayer();
+      if (!FakeMessage.Permissions.has(player, "fakemessage.say")) {
+        return;
+      }
+      split[0] = "";
+      String theguy = split[1];
+      split[1] = "";
+      String message = arrayToString(split, " ");
+      message = removeCharAt(message, 0);
+      String text = messageFormat;
+      text = text.replaceAll("name", theguy);
+      text = text.replaceAll("message", message);
+
+      plugin.getServer().broadcastMessage(text);
+      event.setCancelled(false);
+    }
+    if ((split[0].equalsIgnoreCase("/join")) || 
+      (split[0].equalsIgnoreCase("/j"))) {
+      Player player = event.getPlayer();
+      if (!FakeMessage.Permissions.has(player, "fakemessage.join")) {
+        return;
+      }
+      String theguy = split[1];
+      String text = joingame;
+      text = text.replaceAll("name", theguy);
+      plugin.getServer().broadcastMessage(text);
+      event.setCancelled(false);
+    }
+    if ((split[0].equalsIgnoreCase("/leave")) || 
+      (split[0].equalsIgnoreCase("/l"))) {
+      Player player = event.getPlayer();
+      if (!FakeMessage.Permissions.has(player, "fakemessage.leave")) {
+        return;
+      }
+      String theguy = split[1];
+      String text = leavegame;
+      text = text.replaceAll("name", theguy);
+      plugin.getServer().broadcastMessage(text);
+      event.setCancelled(false);
+    }
+  }
 }
